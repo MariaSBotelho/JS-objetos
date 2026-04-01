c2d27590ef57e730d4275f3e6c06109e586dca3b
Maria Clara Silva Botelho <mariaclarasilvabotelho27@gmail.com>
2026-03-31 22:17:02 -0300

    Entendendo conceito de objetos

---
+++ objeto.js
@@ -0,0 +1,13 @@
+//objetos vs array 
+//array- tem os elementos que são acessados pelo indice
+ const lista = ["maria", "joao", "paulo"];
+
+//array:  const cliente = ["nome","maria","idade",20];
+//objeto  armazena mais de um tipo de informação
+ const cliente = {
+    nome: "maria",
+    idade: 20,
+    cpf:"12312312399",
+    email:"maria@gmail.com"
+ }
+ //objeto: cliente , chave: nome com valor maria, chave idade com valor 20
